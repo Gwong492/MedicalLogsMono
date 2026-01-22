14680b7f7a0c54e01a3144dd62890e5cbf059757
Garrett Wong <123900256+Gwong@users.noreply.github.com>
2026-01-22 10:35:54 -0500

    Added setTitle()

--- symptom-logger-backend/src/headache/headache.controller.js
+++ symptom-logger-backend/src/headache/headache.controller.js
@@ -1,9 +1,16 @@
 const data = require("./headache.service");
 const { buildSymptomReportPdf } = require("../utils/convertPDF");
 const { uploadFileToSlack } = require("../utils/slackUpload");
+const { set } = require("../../app");
 
 const setTitle = (req, res) => {
-  
+  if (req.body.metadata.exportType === "headache") {
+    return "Headache Log";
+  } else if (req.body.metadata.exportType === "bloodPressure") {
+    return "Blood Pressure Log";
+  } else if (req.body.metadata.exportType === "pain") {
+    return "Pain Log";
+  }
 }
 
 const getSymptomData = (req, res) => {
@@ -12,7 +19,7 @@ const getSymptomData = (req, res) => {
 
 const sendPDFReport = async (req, res) => {
   console.log("Title", req.body.metadata.exportType);
-  const pdfBuffer = await buildSymptomReportPdf(req.body.data.logData, { title: req.body.metadata.exportType });
+  const pdfBuffer = await buildSymptomReportPdf(req.body.data.logData, { title: setTitle(req, res) });
   await uploadFileToSlack(pdfBuffer, "headache_report.pdf", req.body.profile.fullName, process.env.SLACK_CHANNEL_ID);
   res.status(200).json({ message: "PDF report sent." });
 }
