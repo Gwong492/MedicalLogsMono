a188597ba8898a66c9925d87f50c96621becec60
Garrett Wong <123900256+Gwong@users.noreply.github.com>
2026-01-21 20:31:41 -0500

    debugging export type

--- symptom-logger-backend/src/headache/headache.controller.js
+++ symptom-logger-backend/src/headache/headache.controller.js
@@ -7,8 +7,8 @@ const getSymptomData = (req, res) => {
 };
 
 const sendPDFReport = async (req, res) => {
-  console.log(req.body.metadata.exportType);
-  res.status(200).json({ message: "logged export type." });
+  console.log(req.body);
+  res.status(200).json({ message: "PDF report sent." });
   // const pdfBuffer = await buildSymptomReportPdf(data.headacheLog, { title: req.body.metadata.exportType });
   // await uploadFileToSlack(pdfBuffer, "headache_report.pdf", process.env.SLACK_CHANNEL_ID);
   // res.status(200).json({ message: "PDF report sent." });
