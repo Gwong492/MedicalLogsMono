b92975f7c9c774d40769f5dd57dfc2123f93d9b3
Garrett Wong <123900256+Gwong@users.noreply.github.com>
2026-01-22 10:30:09 -0500

    Updated debugging for title

--- symptom-logger-backend/src/headache/headache.controller.js
+++ symptom-logger-backend/src/headache/headache.controller.js
@@ -2,12 +2,16 @@ const data = require("./headache.service");
 const { buildSymptomReportPdf } = require("../utils/convertPDF");
 const { uploadFileToSlack } = require("../utils/slackUpload");
 
+const setTitle = (req, res) => {
+  
+}
+
 const getSymptomData = (req, res) => {
   res.json({ data: data });
 };
 
 const sendPDFReport = async (req, res) => {
-  console.log("Data", req.body.profile.fullName);
+  console.log("Title", req.body.metadata.exportType);
   const pdfBuffer = await buildSymptomReportPdf(req.body.data.logData, { title: req.body.metadata.exportType });
   await uploadFileToSlack(pdfBuffer, "headache_report.pdf", req.body.profile.fullName, process.env.SLACK_CHANNEL_ID);
   res.status(200).json({ message: "PDF report sent." });
